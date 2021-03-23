16f6182faaba69d6215b1b627baf51af67fe5226
Ioannis Paraskevakos <g.paraskev@rutgers.edu>
2021-03-23 17:09:09 -0400

    tester

--- tester.py
+++ tester.py
@@ -1,25 +1,20 @@
 import requests
+import sys
+
 from datetime import datetime
 
 def main():
-
     resp = requests.get('https://api.github.com/repos/iparask/git_actions/actions/workflows/dispatch.yml/runs?event=repository_dispatch')
-    print(resp.status_code)
     workflow_json = resp.json()
-    print(workflow_json)
     if workflow_json['total_count'] > 0:
         last_run = workflow_json['workflow_runs'][0]
         finished = last_run['updated_at']
         status = last_run['conclusion']
         diff = datetime.now() - datetime.strptime(finished, '%Y-%m-%dT%H:%M:%SZ')
-        
-        print(diff, status)
         if status == 'success' and diff.seconds > 7:
-            return 1
+            sys.exit(1)
         else:
-            return 0
-    return 1
+            sys.exit(0)
+    sys.exit(1)
 
-
-if '__name__' == '__main__':
-    main()
+main()
