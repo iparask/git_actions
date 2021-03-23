1ada8d42e6fec89f3eae5708448978bc208cab4c
Ioannis Paraskevakos <g.paraskev@rutgers.edu>
2021-03-23 15:50:21 -0400

    updating workflow

--- tester.py
+++ tester.py
@@ -1,17 +1,25 @@
 import requests
 from datetime import datetime
 
-if '__name__' == '__main__':
+def main():
+
     resp = requests.get('https://api.github.com/repos/iparask/git_actions/actions/workflows/dispatch.yml/runs?event=repository_dispatch')
-
+    print(resp.status_code)
     workflow_json = resp.json()
-
-    if workflow_json['total_count']:
+    print(workflow_json)
+    if workflow_json['total_count'] > 0:
         last_run = workflow_json['workflow_runs'][0]
         finished = last_run['updated_at']
         status = last_run['conclusion']
         diff = datetime.now() - datetime.strptime(finished, '%Y-%m-%dT%H:%M:%SZ')
-        if status == 'success' and diff.days > 7:
+        
+        print(diff, status)
+        if status == 'success' and diff.seconds > 7:
             return 1
         else:
             return 0
+    return 1
+
+
+if '__name__' == '__main__':
+    main()
