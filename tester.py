db34cfed3703a551ba96bb4836db0df2cef4c381
Ioannis Paraskevakos <g.paraskev@rutgers.edu>
2021-03-23 15:16:32 -0400

    update workflow

--- tester.py
+++ tester.py
@@ -10,7 +10,7 @@ if '__name__' == '__main__':
         last_run = workflow_json['workflow_runs'][0]
         finished = last_run['updated_at']
         status = last_run['conclusion']
-        diff = datetime.now() - datetime.strptime(finished, '%Y-%m-%dT%H:%M:%SZ'))
+        diff = datetime.now() - datetime.strptime(finished, '%Y-%m-%dT%H:%M:%SZ')
         if status == 'success' and diff.days > 7:
             return 1
         else:
